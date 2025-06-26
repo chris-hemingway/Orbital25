6dee92642578ed0d43e8f3ead8de81a2bcbba15f
Christian <christ.hemingway@gmail.com>
2025-06-26 14:15:48 +0800

    initial docker

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,16 @@ const productRoutes = require('./routes/productRoutes');
 const wishlistRoutes = require('./routes/wishlistRoutes')
 
 const app = express();
+const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
