7997d6ad74d1445f6f5eeaea0cf47ce9d8a7c0fe
Christian <christ.hemingway@gmail.com>
2025-06-29 14:34:20 +0800

    wishlist schema update

--- backend/models/Wishlist.js
+++ backend/models/Wishlist.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const wishlistSchema = new mongoose.Schema({
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     product_id: { type: Number, required: true }, 
-    added_at: { type: Date, default: Date.now },
-  });
+    added_at: { type: Date, default: Date.now }
+}, { versionKey: false }) ;
 
 
 module.exports = mongoose.model('Wishlist', wishlistSchema);
